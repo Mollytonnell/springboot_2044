6f97f72f1bf2ac89e302a6e7a81d2207341f3e7d
GBTC440007UR <GBTC440007UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-03-25 19:29:57 -0400

    Initial commit

---
+++ src/main/java/com/bymolly/springboot_2044/Springboot2044Application.java
@@ -0,0 +1,13 @@
+package com.bymolly.springboot_2044;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2044Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2044Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bymolly/springboot_2044/Springboot2044ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bymolly.springboot_2044;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot2044ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
